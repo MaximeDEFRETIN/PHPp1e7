826fc564153f42032aaeeb6268db2dea455fd051
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 08:54:28 +0100

    Correction

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<?php
-$name = "Błaszczykowski";
-$firstName = "Jakub";
-$age = 31;
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partiee 1 : exercice 7</title>
+  </head>
+  <body>
+    <?php
+    $name = "Błaszczykowski";
+    $firstName = "Jakub";
+    $age = 31;
 
-echo "Bonjour $firstName $name, tu as $age ans."
-?>
+    echo 'Bonjour ' . $firstName  . $name . ', tu as ' . $age . ' ans.';
+    ?>
+  </body>
+</html>
